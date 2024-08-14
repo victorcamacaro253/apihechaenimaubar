56e98d8b8327b4ea6a7936da075089c66962e7d1
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-08-14 11:45:00 -0400

    Add files via upload

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const {check,validationResult} = require('express-validator');
+
 
 //Ruta para obtener los datos de la base de datos
 
@@ -13,5 +15,7 @@ router.post('/users',userController.addUser);
 router.get('/users/:id',userController.getUserById);
 
 router.put('/users/:id', userController.updateUser);
+
+router.delete('/users/:id', userController.deleteUser);
 
 module.exports = router;
