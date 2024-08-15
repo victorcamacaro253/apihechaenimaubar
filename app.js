1da6145e2be7760010d08c29db128b83292403c3
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-08-15 10:49:22 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const userRoutes = require('./routes/userRoutes')
-
 const app = express();
-app.use(express.json());
 
+
+
+app.use(express.json());
 app.disable('x-powered-by')
 
 app.get('/',(req,res)=>{
     res.json({ message : 'hola mundo' })
 })
 
+
+app.options('/api/users/:id', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.sendStatus(200);
+});
 //Usa las rutas de usuarios 
 app.use('/api',userRoutes);
 
