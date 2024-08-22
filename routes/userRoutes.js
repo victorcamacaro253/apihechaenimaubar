5d04d9093e548e89b90aa64ae949977c78bfe920
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-08-22 10:20:41 -0400

    Add files via upload

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-
+const authenticateToken = require('../middleware/authenticationToken');
 
 
 //Ruta para obtener los datos de la base de datos
@@ -9,6 +9,7 @@ const userController = require('../controllers/userController');
 router.get('/users',userController.getAllUser);
 
 router.get('/users/searchUser',userController.searchUsers);
+router.get('/users/getperfil',authenticateToken,userController.getPerfil)
 
 //Ruta para agregar un nuevo usuario
 
@@ -17,6 +18,8 @@ router.post('/users',userController.addUser);
 router.post('/users/login', userController.loginUser);
 
 router.get('/users/:id',userController.getUserById);
+
+
 
 router.put('/users/:id', userController.updateUser);
 
