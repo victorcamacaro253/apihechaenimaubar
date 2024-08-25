6bf2d56080730b2b9aaa59dac732f9644973e7da
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-08-25 16:00:08 -0400

    Add files via upload

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const authenticateToken = require('../middleware/authenticationToken');
+const productController = require('../controllers/productController');
 
 
 //Ruta para obtener los datos de la base de datos
@@ -10,8 +11,11 @@ router.get('/users',userController.getAllUser);
 
 router.get('/users/searchUser',userController.searchUsers);
 router.get('/users/getperfil',authenticateToken,userController.getPerfil)
-router.get('/users/getProducts',userController.getProducts);
-router.get('/users/getProductById/:id',userController.getProductById);
+
+router.get('/users/getProducts',productController.getProducts)
+router.get('/users/getProducts/:id',productController.getProductsById)
+
+
 //Ruta para agregar un nuevo usuario
 
 router.post('/users',authenticateToken,userController.addUser);
@@ -20,6 +24,7 @@ router.post('/users/login', userController.loginUser);
 
 router.get('/users/:id',userController.getUserById);
 
+router.post('/users/addProduct',productController.addProduct);
 
 router.put('/users/:id', userController.updateUser);
 
