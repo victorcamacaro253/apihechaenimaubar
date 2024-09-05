46b2e1da3c3eaa3b0059ecea2648804f5e1ed5f5
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-09-05 15:52:24 -0400

    Add files via upload

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = Router();
 import userController from '../controllers/userController.js';
 import authenticateToken from '../middleware/authenticationToken.js';
 import productController from '../controllers/productController.js';
-
+import comprasController from '../controllers/comprasController.js';
 
 //Ruta para obtener los datos de la base de datos
 
@@ -13,9 +13,9 @@ router.get('/users/searchUser',userController.searchUsers);
 router.get('/users/getperfil',authenticateToken ,userController.getPerfil)
 
 
-router.get('/products',productController.getProducts)
-router.get('/products/:id',productController.getProductsById)
-router.get('/users/getUserPerfil/:id',authenticateToken ,userController.getUserPerfil)
+router.get('/Products',productController.getProducts)
+router.get('/Products/:id',productController.getProductsById)
+router.get('/users/getUserPerfil/:id',userController.getUserPerfil)
 
 //Ruta para agregar un nuevo usuario
 
@@ -25,17 +25,17 @@ router.post('/users/login', userController.loginUser);
 
 router.get('/users/:id',userController.getUserById);
 
-router.post('/products',productController.addProduct);
+router.post('/Products',productController.addProduct);
+router.post('/compra',comprasController.compraProduct);
+router.delete('/compra/:id',comprasController.deleteCompra);
 
 router.put('/users/:id', userController.updateUser);
 
-router.put('/products/:id',productController.updateProduct)
-
 router.delete('/users/:id', userController.deleteUser);
+router.delete('/products/:id',productController.deleteProduct)
 
 router.patch('/users/:id', userController.partialUpdateUser)
 
-router.delete('/products/:id',productController.deleteProduct)
 
 
 export default router;
