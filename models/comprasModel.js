18ab53f1bd8a9bdee934d6ec2f782e04402f8ed7
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-09-09 14:43:03 -0400

    Add files via upload

--- models/comprasModel.js
+++ models/comprasModel.js
@@ -2,6 +2,20 @@ import { query } from '../db/db1.js';
 
 const comprasModel= {
 
+
+
+    async getCompras(){
+        const result= await query('SELECT * FROM compras INNER JOIN productos ON compras.id_producto=productos.id_producto INNER JOIN usuario ON compras.id_usuario=usuario.id')
+        return result;
+    },
+
+    
+    async getCompraById(id){
+        const result= await query('SELECT * FROM compras INNER JOIN productos ON compras.id_producto=productos.id_producto INNER JOIN usuario ON compras.id_usuario=usuario.id WHERE compras.id_compra=?',[id]);
+
+        return result;
+    },
+
 async addCompra(connection,id_producto,cantidad,id_usuario){
     const [result]= await connection.query(' INSERT INTO compras (id_producto, cantidad, fecha, id_usuario) VALUES (?, ?, NOW(), ?)',
             [id_producto, cantidad, id_usuario])
@@ -13,7 +27,9 @@ async addCompra(connection,id_producto,cantidad,id_usuario){
 
 async deleteCompra(id){
 
-    const result = await query('DELETE FROM productos ')
+    const result = await query('DELETE FROM compras WHERE id_compra=?',[id])
+        return result;
+    
 }
 
 }
