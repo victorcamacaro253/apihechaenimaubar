9133d329026c844a2284efbd722c24f1767560a1
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-09-17 14:55:16 -0400

    Add files via upload

--- models/notificationsModel.js
+++ models/notificationsModel.js
@@ -14,13 +14,38 @@ async  getNotifications(){
     return results;
 },
 
+async getNotificationById(id){
+    const results = await query('SELECT * FROM notificaciones WHERE id=?',[id]);
+    return results;
+}
+,
 
 // Función para guardar una notificación en la base de datos
 async saveNotification  (message,type)  {
     
     const results = await query('INSERT INTO notificaciones (message,created_at,type) VALUES (?, NOW(),?)', [message,type]);
     return results.insertId;
-}
+},
+
+    
+
+async updateNotification (notificacionId,updateFields){
+
+
+   try {
+    
+    const queryStr = 'UPDATE notificaciones SET ? WHERE id = ?';
+    
+    const result = await query(queryStr,[updateFields,notificacionId]);
+    return result;
+
+   } catch (error) {
+    console.error('Error al obtener todos los usuarios:', error);
+    throw new Error('Error al obtener todos los usuarios');
+   }
+
+    
+    },
 
 
 
