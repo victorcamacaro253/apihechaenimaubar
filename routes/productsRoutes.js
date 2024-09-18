67306d0ba543371154cfa81540db49bf1e19a65f
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-09-17 23:10:44 -0400

    Add files via upload

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -4,6 +4,9 @@ import productController from '../controllers/productController.js';
 
 const router = Router();
 
+//Ruta para obtener los productos filtrado por nombre
+router.get('/products/searchProductByName',productController.searchProductByName)
+
 //Ruta para obtener todos los productos
 router.get('/products',productController.getProducts)
 
@@ -11,8 +14,7 @@ router.get('/products',productController.getProducts)
 //Ruta para obtener los productos por id
 router.get('/products/:id',productController.getProductsById)
 
-//Ruta para obtener los productos filtrado por nombre
-router.get('/products/searchProductByName',productController.searchProductByName)
+
 
 //Ruta para añadir un producto
 
@@ -27,6 +29,7 @@ router.put('/products/:id',productController.updateProduct)
 //Ruta para eliminar el producto
 
 router.delete('/products/:id',productController.deleteProduct)
+
 
 
 
