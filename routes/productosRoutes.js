57be4bbd9098d7f1073e1e061af9b36d83bd5d8d
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-09-23 15:50:20 -0400

    Add files via upload

--- routes/productosRoutes.js
+++ routes/productosRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import productController from "../controllers/productController.js"
 import authenticateToken from "../middleware/authenticationToken.js"
+import upload from '../middleware/multerConfig.js'
 
 const router = Router()
 
@@ -9,12 +10,20 @@ const router = Router()
 
 router.get('/Products',productController.getProducts)
 
+router.get('/Products/categoria/',productController.getProductsByCategoria)
+
+router.get('/Products/price',productController.getProductsByPrinceRange)
+
+router.post('/Products/addMultipleProducts',upload.single('image'),productController.addMultipleProducts)
+
 
 router.get('/Products/:id',productController.getProductsById)
 
-router.post('/Products',productController.addProduct);
 
-router.delete('/products/:id',productController.deleteProduct)
+router.post('/Products',upload.single('image'),productController.addProduct);
+
+
+router.delete('/Products/:id',productController.deleteProduct)
 
 
 export default router ;
