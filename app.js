ff189ebfc1bb6a549f728b4693aa0d2a61df12e6
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-09-27 20:26:49 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,50 +1,24 @@
-import express, { json } from 'express';
-import empleadosRoutes from './routes/EmpleadosRoutes.js';
-import administradoresRoutes from './routes/administradoresRoutes.js'
-import bienesRoutes from './routes/bienesRoutes.js'
-import helmet from 'helmet';
-import cors from 'cors'
-import path from 'path';
-import { fileURLToPath } from 'url';
-import cookieParser from 'cookie-parser'; // <-- Agrega esto
-import csrf from 'csurf'; // <-- Agrega esto también
-
-
-// Get the current file's directory (equivalent to __dirname in CommonJS)
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
+const express = require('express')
+const userRoutes = require('./routes/userRoutes')
 const app = express();
 
-app.use(cors())
-// Añadir protección CSRF con cookies
-const csrfProtection = csrf({ cookie: true });
-// Middleware para procesar cookies
-app.use(cookieParser());
 
-
-app.use(json());
+app.use(express.json());
 app.disable('x-powered-by')
 
-app.use(helmet());
+app.get('/',(req,res)=>{
+    res.json({ message : 'hola mundo' })
+})
 
-app.use('/uploads',express.static(path.join(__dirname,'uploads')));
 
-
-// Generar y enviar el token CSRF en una ruta
-app.get('/csrf-token', csrfProtection, (req, res) => {
-    // Envía el token CSRF en una cookie llamada 'XSRF-TOKEN'
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.json({ csrfToken: req.csrfToken() });
-  });
-  
-
-app.use('/api',csrfProtection,empleadosRoutes);
-
-app.use('/api2',administradoresRoutes);
-
-app.use('/api3',bienesRoutes);
+app.options('/api/users/:id', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.sendStatus(200);
+});
+//Usa las rutas de usuarios 
+app.use('/api',userRoutes);
 
 
 const PORT = process.env.PORT ?? 3001
