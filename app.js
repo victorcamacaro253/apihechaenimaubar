4c1107a12a98ea50c9da89494e0176d12334f7c8
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-09-27 20:27:51 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,56 @@
-const express = require('express')
-const userRoutes = require('./routes/userRoutes')
+import express, { json } from 'express';
+import userRoutes from './routes/userRoutes.js';
+import productsRoutes from './routes/productsRoutes.js'
+import comprasRoutes from './routes/comprasRoutes.js'
+import limiter from './rateLimiter.js';
+import helmet from 'helmet';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import csrf from 'csurf';
+ 
+
 const app = express();
 
 
-app.use(express.json());
+
+
+app.use(helmet())
+
+/*
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "https://cdn.jsdelivr.net"],
+      styleSrc: ["'self'", "https://fonts.googleapis.com"],
+      fontSrc: ["'self'", "https://fonts.gstatic.com"],
+      imgSrc: ["'self'", "https://example.com"],
+      objectSrc: ["'none'"],
+      upgradeInsecureRequests: true,
+    },
+  }))
+*//
+app.use(cors())
+app.use(json());
+app.use(limiter);
+
 app.disable('x-powered-by')
+
+// Añadir protección CSRF con cookies
+const csrfProtection = csrf({ cookie: true });
+// Middleware para procesar cookies
+app.use(cookieParser());
 
 app.get('/',(req,res)=>{
     res.json({ message : 'hola mundo' })
 })
 
+
+// Generar y enviar el token CSRF en una ruta
+app.get('/csrf-token', csrfProtection, (req, res) => {
+  // Envía el token CSRF en una cookie llamada 'XSRF-TOKEN'
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.json({ csrfToken: req.csrfToken() });
+});
 
 app.options('/api/users/:id', (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -17,11 +58,21 @@ app.options('/api/users/:id', (req, res) => {
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.sendStatus(200);
 });
+
+
 //Usa las rutas de usuarios 
-app.use('/api',userRoutes);
+app.use('/api',csrfProtection,userRoutes);
 
 
-const PORT = process.env.PORT ?? 3001
+//Usa las rutas de productos
+app.use('/api2',productsRoutes);
+
+
+//Usa las rutas de las compras
+app.use('/api3',comprasRoutes);
+
+
+const PORT = process.env.PORT ?? 3000
 
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
