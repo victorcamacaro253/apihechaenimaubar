07895b04d0115494c2d31dc042aff24a8d684d16
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-09-29 16:06:23 -0400

    Add files via upload

--- models/userModels.js
+++ models/userModels.js
@@ -43,7 +43,40 @@ const UserModel = {
     
   return insertedUser[0]; // Asegúrate de retornar solo el primer resultado
     },
+
+    async addUserFacebook({ facebook_id, nombre, correo, imagen }) {
+        const result = await _query('INSERT INTO usuario (facebook_id, nombre, correo, imagen) VALUES (?, ?, ?, ?)',
+            [facebook_id, nombre, correo, imagen]
+        );
     
+     // Ahora, busca el usuario insertado usando su ID
+  const insertedUser = await _query('SELECT * FROM usuario WHERE id = ?', [result.insertId]);
+    
+  return insertedUser[0]; // Asegúrate de retornar solo el primer resultado
+    },
+
+    async addUserGithub({ github_id, nombre, correo, imagen }) {
+        const result = await _query('INSERT INTO usuario (github_id, nombre, correo, imagen) VALUES (?, ?, ?, ?)',
+            [github_id, nombre, correo, imagen]
+        );
+    
+     // Ahora, busca el usuario insertado usando su ID
+  const insertedUser = await _query('SELECT * FROM usuario WHERE id = ?', [result.insertId]);
+    
+  return insertedUser[0]; // Asegúrate de retornar solo el primer resultado
+    },
+
+    
+    async addUserTwitter({ twitter_id, nombre, correo, imagen }) {
+        const result = await _query('INSERT INTO usuario (twitter_id, nombre, correo, imagen) VALUES (?, ?, ?, ?)',
+            [twitter_id, nombre, correo, imagen]
+        );
+    
+     // Ahora, busca el usuario insertado usando su ID
+  const insertedUser = await _query('SELECT * FROM usuario WHERE id = ?', [result.insertId]);
+    
+  return insertedUser[0]; // Asegúrate de retornar solo el primer resultado
+    },
     
     // Modelo: updateUser
 async updateUser(id, updateFields, values) {
@@ -182,6 +215,21 @@ async getUsersWithPagination(limit,offset){
     async findUserByGoogleId(googleId) {
         const query = 'SELECT * FROM usuario WHERE google_id = ?';
         const [rows] = await _query(query, [googleId]);
+        return rows;
+    },
+    async findUserByFacebookId(facebookId) {
+        const query = 'SELECT * FROM usuario WHERE google_id = ?';
+        const [rows] = await _query(query, [facebookId]);
+        return rows;
+    },
+    async findUserByGithubId(githubId) {
+        const query = 'SELECT * FROM usuario WHERE github_id = ?';
+        const [rows] = await _query(query, [githubId]);
+        return rows;
+    },
+    async findUserByTwitterId(twitterId) {
+        const query = 'SELECT * FROM usuario WHERE twitter_id = ?';
+        const [rows] = await _query(query, [twitterId]);
         return rows;
     }
 
