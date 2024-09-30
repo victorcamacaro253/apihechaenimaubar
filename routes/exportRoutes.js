7cef77bbaaf94895980d356cffaef3389db2f230
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-09-30 08:40:44 -0400

    Add files via upload

--- routes/exportRoutes.js
+++ routes/exportRoutes.js
@@ -15,4 +15,8 @@ router.get('/export/PDF/users', exportControllers.exportUserDataPdf)
 
 router.get('/export/PDF/users/:id', exportControllers.exportUserDataByIdPdf)
 
+router.get('/export/CSV/users',exportControllers.exportUserDataToCsv)
+
+router.get('/export/CSV/users/:id',exportControllers.exportUserDataToCsvByid)
+
 export default router
