f5a56226da84d0ac7029efa2891ff5d320e7368f
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-10-07 15:48:45 -0400

    Add files via upload

--- models/comprasModel.js
+++ models/comprasModel.js
@@ -11,7 +11,7 @@ const comprasModel= {
 
     
     async getCompraById(id){
-        const result= await query('SELECT * FROM compras INNER JOIN productos ON compras.id_producto=productos.id_producto INNER JOIN usuario ON compras.id_usuario=usuario.id WHERE compras.id_compra=?',[id]);
+        const result= await query('SELECT * FROM compras INNER JOIN usuario ON compras.id_usuario=usuario.id WHERE compras.id_compra=?',[id]);
 
         return result;
     },
@@ -51,6 +51,47 @@ async findByDateRange (startDate, endDate){
         WHERE fecha BETWEEN ? AND ?
     `;
     const results = await query(SQL, [startDate, endDate]);
+    return results;
+},
+
+async getComprasByUserId(id){
+
+ const SQL = `  SELECT 
+      c.id_compra, 
+      c.fecha, 
+      u.id AS id_usuario, 
+      u.nombre, 
+      u.apellido, 
+      u.cedula, 
+      u.correo, 
+      mp.id_producto, 
+      mp.cantidad, 
+      mp.precio,
+      p.nombre_producto
+    FROM 
+      productos_compras AS mp 
+
+      JOIN productos p ON  mp.id_producto = p.id_producto
+    JOIN 
+      compras c ON mp.id_compra = c.id_compra 
+    JOIN 
+      usuario u ON c.id_usuario = u.id 
+    WHERE 
+      c.id_usuario = ?;
+
+ `;
+
+ const results= await query(SQL,[id])
+
+ return results;
+
+},
+
+async getComprasByUsername(nombre){
+
+    const SQL= 'SELECT * FROM `productos_compras` as mp JOIN compras c ON mp.id_compra= c.id_compra JOIN usuario u ON c.id_usuario=u.id  WHERE u.nombre = ?';
+    const results= await query(SQL,[nombre])
+
     return results;
 }
 
