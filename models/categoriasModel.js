0c0f8edbdf2da2db236fda34a944e632ba42c06a
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-10-15 18:16:14 -0400

    Add files via upload

---
+++ models/categoriasModel.js
@@ -0,0 +1,19 @@
+import { query } from "../db/db1.js";
+
+const categoriasModel ={
+
+
+ async getCategories(){
+ 
+    const SQL="SELECT * FROM categorias ";
+
+    const result = query(SQL);
+    return result;
+
+ }
+
+
+}
+
+
+export default categoriasModel;
