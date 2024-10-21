7858199df27d915b6df711d0f6c2984b30a05c64
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-10-21 19:06:30 -0400

    Add files via upload

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
 import paymentControllers from "../controllers/paymentControllers.js";
+
 const router = Router();
 
+//crear pago con stripe
 router.post('/stripe',paymentControllers.paymentStripe)
+
+//crear un  pago con mercadopago
 
 router.post('/mercadopago',paymentControllers.paymentMercadopago)
 
+//crear pago con paypal
+router.post('/paypal/createPayment',paymentControllers.createPaymentPaypal)
+
+//capturar el pago despues de la aprobacion del cliente
+//router.post('/paypal/capture-payment',paymentControllers.capturePaymentPaypal)
 
 export default router
