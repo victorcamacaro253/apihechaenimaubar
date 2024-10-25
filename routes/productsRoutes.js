e42eb9356a636e0e5a740f69149e2a277c206226
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-10-24 23:34:58 -0400

    Add files via upload

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -5,42 +5,42 @@ import upload from '../middleware/multerConfig.js';
 const router = Router();
 
 //Ruta para obtener los productos filtrado por nombre
-router.get('/products/searchProductByName',productController.searchProductByName)
+router.get('/searchProductByName',productController.searchProductByName)
 
 //Ruta para obtener todos los productos
-router.get('/products',productController.getProducts)
+router.get('/',productController.getProducts)
 
 //Ruta para obtener los porductos por categoria
-router.get('/products/categoria/',productController.getProductsByCategoria)
+router.get('/categoria/',productController.getProductsByCategoria)
 
 //Ruta para obtener los productos por un rango de precio
-router.get('/products/price',productController.getProductsByPriceRange)
+router.get('/price',productController.getProductsByPrinceRange)
 
 
 //Ruta para obtener los productos por id
-router.get('/products/:id',productController.getProductsById)
+router.get('/:id',productController.getProductsById)
 
 //Ruta para ingresar multiples productos
-router.post('/products/addMultipleProducts',upload.single('image'),productController.addMultipleProducts)
+router.post('/addMultipleProducts',upload.single('image'),productController.addMultipleProducts)
 
 
 //Ruta para añadir un producto
 
-router.post('/products',upload.single('image'),productController.addProduct);
+router.post('/',upload.single('image'),productController.addProduct);
 
 
 //Ruta para actualizar un producto
 
-router.put('/products/:id',productController.updateProduct)
+router.put('/:id',productController.updateProduct)
 
 //Ruta para eliminar multiples productos
-router.post('/products/deleteMultipleProducts',productController.deleteMultipleProducts)
+router.post('/deleteMultipleProducts',productController.deleteMultipleProducts)
 
 
 
 //Ruta para eliminar el producto
 
-router.delete('/products/:id',productController.deleteProduct)
+router.delete('/:id',productController.deleteProduct)
 
 
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,74 +2,74 @@ import { Router } from 'express';
 import userController from '../controllers/userController.js';
 import authenticateToken from '../middleware/authenticationToken.js';
 import upload from '../middleware/multerConfig.js';
-
+import checkPermissions from '../middleware/checkPermission.js';
 
 const router = Router();
 
 //Ruta para obtener los datos de la base de datos
 
 //Ruta para obtener todos los usuarios de la base de datos
-router.get('/users',userController.getAllUser);
+router.get('/',userController.getAllUser);
 
 
 //Ruta para obtener los usuarios filtrados
-router.get('/users/searchUser',userController.searchUsers);
+router.get('/searchUser',userController.searchUsers);
 
 //Ruta para obtener el perfi de los usuarios
-router.get('/users/getperfil',authenticateToken ,userController.getPerfil)
+router.get('/getperfil',authenticateToken,checkPermissions('read') ,userController.getPerfil)
 
 
 
 //Ruta para obtener el historial de ingresos del usuario por el nombre
-router.get('/users/loginHistorial',userController.getLoginHistory)
+router.get('/loginHistorial',userController.getLoginHistory)
 
 
 
 //Ruta para obtener el perfil del usuario
-router.get('/users/getUserPerfil/:id',authenticateToken ,userController.getUserPerfil)
+router.get('/getUserPerfil/:id',authenticateToken ,userController.getUserPerfil)
 
 
 //Ruta para obtener los usarios con paginacion
-router.get('/users/pagination',userController.getUsersWithPagination)
+router.get('/pagination',userController.getUsersWithPagination)
 
 
 //Ruta para agregar un nuevo usuario
 
-router.post('/users',authenticateToken,userController.addUser);
+router.post('/',authenticateToken,userController.addUser);
 
 //Ruta para logearse
 
-router.post('/users/login', userController.loginUser);
+router.post('/login', userController.loginUser);
 
 
 //Ruta para obetenr un usuario por id
 
-router.get('/users/:id',userController.getUserById);
+router.get('/:id',userController.getUserById);
 
 
 //Ruta para insertar multiples usuarios
-router.post('/users/addMultipleUsers',userController.addMultipleUsers)
+router.post('/addMultipleUsers',userController.addMultipleUsers)
 
 //Ruta para eliminar multiple usuarios
-router.post('/users/deleteMultipleUsers',userController.deleteMultipleUsers)
+router.post('/deleteMultipleUsers',userController.deleteMultipleUsers)
 
 //Ruta para solicitar el password reset
-router.post('/users/requestPasswordRequest',userController.requestPasswordReset)
+router.post('/requestPasswordRequest',userController.requestPasswordReset)
 
 //Ruta para resetear el password
-router.post('/users/resetPassword/:token',userController.resetPassword)
+router.post('/resetPassword/:token',userController.resetPassword)
 
 //Ruta para actualizar un usuario
 
-router.put('/users/:id', userController.updateUser);
+router.put('/:id', userController.updateUser);
 
 //Ruta para eliminar un producto
 
-router.delete('/users/:id', userController.deleteUser);
+router.delete('/:id', userController.deleteUser);
 
 //Ruta para actualizar un producto parcial
 
-router.patch('/users/:id', userController.partialUpdateUser)
+router.patch('/:id', userController.partialUpdateUser)
 
 
 export default router;
