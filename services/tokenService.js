1a347877f86df4525605a529334f0a3611b0614d
Victor C <victorcamacaro253@gmail.com>
2024-11-22 01:22:45 -0400

    Add files via upload

--- services/tokenService.js
+++ services/tokenService.js
@@ -1,23 +1,35 @@
 import jwt from 'jsonwebtoken'
-
-const generateToken=(UserId)=>{
+import { User } from 'mercadopago';
+/*
+const generateToken=(UserId,email,role)=>{
    
   // Generar un token JWT
   const token = jwt.sign(
-    { id: UserId},
+    { id: UserId,email:email,rol:role},
     process.env.JWT_SECRET, // Asegúrate de tener JWT_SECRET en tus variables de entorno
     { expiresIn: '1h' } // Expiración del token, por ejemplo, 1 hora
 );
 
 return token;
 }
+*/
+
+const generateToken = (UserId,email,role,expiresIn)=>{
+  const payload = {id:UserId,email:email,rol:role }
+
+const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: expiresIn });
+return token;
+}
+
+
 
 const verifyToken=(token)=>{
-     try{
+  
+  try{
 
   const decode = jwt.verify(token, process.env.JWT_SECRET);
   return decode.id; //Retorna el id del usuario 
-}catch(error){
+  }catch(error){
     return null  
   }
 
